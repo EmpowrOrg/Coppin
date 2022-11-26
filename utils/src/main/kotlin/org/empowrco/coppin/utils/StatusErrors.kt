d63bb7134ecebac6d6d0944adb3540f75fc81d9a
adevezin <adrianldevezin@mailfence.com>
2022-11-26 15:22:27 -0500

    fix returned errors

--- utils/src/main/kotlin/org/empowrco/coppin/utils/StatusErrors.kt
+++ utils/src/main/kotlin/org/empowrco/coppin/utils/StatusErrors.kt
@@ -7,6 +7,6 @@ class LanguageSupportException(language: String) :
     RuntimeException("$language does not support the necessary features.")
 
 class AssignmentLanguageSupportException(language: String) :
-    RuntimeException("The assignmnet does not support $language")
+    RuntimeException("The assignment does not support $language")
 
 class InvalidUuidException(property: String) : RuntimeException("Invalid uuid specified for property $property")
