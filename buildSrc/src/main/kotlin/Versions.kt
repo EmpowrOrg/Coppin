0dbc1da0fd6a9801f0ecf55a11ee1865150c7729
adevezin <adrianldevezin@mailfence.com>
2023-03-11 13:03:01 -0500

    Update versions

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,8 +1,9 @@
 import org.gradle.kotlin.dsl.provideDelegate
 
 object Versions {
+    const val jwt = "4.2.2"
     const val kotlin = "1.7.20"
-    const val ktor = "2.1.2"
+    const val ktor = "2.2.2"
     const val logback = "1.2.10"
     const val koin = "3.2.2"
     const val exposed = "0.40.1"
