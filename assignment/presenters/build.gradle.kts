6e6197e15baaed81ee7b2a6a1b3e7efd089d3319
adevezin <adrianldevezin@mailfence.com>
2023-03-11 13:06:19 -0500

    Remove unneeded utils

--- assignment/presenters/build.gradle.kts
+++ assignment/presenters/build.gradle.kts
@@ -5,7 +5,6 @@ plugins {
 dependencies {
     implementation(Deps.Koin.main)
     implementation(project(Deps.Models.main))
-    implementation(project(Deps.Utils.routing))
     implementation(Deps.Kotlin.dateTime)
     implementation(Deps.Ktor.json)
     implementation(project(Deps.Assignment.Backend.main))
