e1f941bb0a772896f51e20606aed77aeb596dc13
adevezin <adrianldevezin@mailfence.com>
2023-03-14 11:08:07 -0400

    Delete migrations

--- db/src/main/kotlin/org/empowrco/coppin/db/Migrations.kt
+++
@@ -1,7 +0,0 @@
-package org.empowrco.coppin.db
-
-internal val databaseVersion = 1
-
-internal val migrations = listOf(
-    "ALTER TABLE assignments RENAME COLUMN assignment_id TO reference_id;",
-)
