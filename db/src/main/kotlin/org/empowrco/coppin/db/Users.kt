27b2ccb4d3de0ca228f956b57c2dd1899c33c051
adevezin <adrianldevezin@mailfence.com>
2023-03-19 22:47:06 -0400

    Make user email unique

--- db/src/main/kotlin/org/empowrco/coppin/db/Users.kt
+++ db/src/main/kotlin/org/empowrco/coppin/db/Users.kt
@@ -5,7 +5,7 @@ import org.empowrco.coppin.models.User
 object Users : BaseTable() {
     val firstName = varchar("first_name", ColumnConfig.name)
     val lastName = varchar("last_name", ColumnConfig.name)
-    val email = varchar("email", ColumnConfig.username)
+    val email = varchar("email", ColumnConfig.username).uniqueIndex()
     val passwordHash = varchar("password_hash", ColumnConfig.password)
     val type = enumeration<User.Type>("type")
     val isAuthorized = bool("is_authorized")
