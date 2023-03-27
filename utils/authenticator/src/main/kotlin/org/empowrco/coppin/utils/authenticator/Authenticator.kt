4a1cd5f83c4cf78e09286ff67e5d6984e7ac179e
adevezin <adrianldevezin@mailfence.com>
2023-03-27 11:27:22 -0400

    Add method to validate keys

--- utils/authenticator/src/main/kotlin/org/empowrco/coppin/utils/authenticator/Authenticator.kt
+++ utils/authenticator/src/main/kotlin/org/empowrco/coppin/utils/authenticator/Authenticator.kt
@@ -1,24 +1,18 @@
 package org.empowrco.coppin.utils.authenticator
 
-import com.auth0.jwt.JWT
-import com.auth0.jwt.JWTVerifier
-import com.auth0.jwt.algorithms.Algorithm
 import io.ktor.server.auth.UserIdPrincipal
-import io.ktor.server.auth.jwt.JWTCredential
 import io.ktor.util.hex
 import org.empowrco.coppin.sources.UsersSource
+import java.util.Base64
 import java.util.UUID
 import javax.crypto.Mac
 import javax.crypto.spec.SecretKeySpec
 
 interface Authenticator {
-    val verifier: JWTVerifier
-    suspend fun validate(email: String, password: String): UserIdPrincipal?
-    suspend fun validate(jwt: JWTCredential): UserIdPrincipal?
     suspend fun validateSession(id: String): UserIdPrincipal?
+    suspend fun validateKey(accessKey: String): UserIdPrincipal?
     suspend fun hash(password: String): String
     suspend fun isValidPassword(password: String): Result<Boolean>
-
 }
 
 internal class RealAuthenticator(
@@ -27,33 +21,7 @@ internal class RealAuthenticator(
 
     private val hashKey = hex(System.getenv("SECRET_KEY"))
     private val algorithm = "HmacSHA1"
-    private val hmacKey = SecretKeySpec(hashKey, algorithm)
-    private val issuer = "adminServer"
-    private val jwtSecret = System.getenv("JWT_SECRET") // 1
-    private val jwtAlgorithm = Algorithm.HMAC512(jwtSecret)
-    override val verifier: JWTVerifier = JWT
-        .require(jwtAlgorithm)
-        .withIssuer(issuer)
-        .build()
 
-    override suspend fun validate(email: String, password: String): UserIdPrincipal? {
-        val user = usersSource.getUserByEmail(email) ?: return null
-        val passwordHash = hash(password)
-        return if (user.passwordHash == passwordHash) {
-            return UserIdPrincipal(user.email)
-        } else {
-            null
-        }
-    }
-
-    override suspend fun validate(jwt: JWTCredential): UserIdPrincipal? {
-        val payload = jwt.payload
-        val claim = payload.getClaim("id")
-        val claimString = claim.asString()
-        val userUuid = UUID.fromString(claimString) ?: return null
-        val user = usersSource.getUser(userUuid) ?: return null
-        return UserIdPrincipal(user.email)
-    }
 
     override suspend fun validateSession(id: String): UserIdPrincipal? {
         val uuid = UUID.fromString(id) ?: return null
@@ -64,8 +32,31 @@ internal class RealAuthenticator(
         return UserIdPrincipal(user.email)
     }
 
+    override suspend fun validateKey(accessKey: String): UserIdPrincipal? {
+        val keyParts = accessKey.split(".")
+        if (keyParts.size != 2) {
+            return null
+        }
+        val keyIdString = Base64.getDecoder().decode(keyParts.first()).decodeToString()
+        val keyId = try {
+            UUID.fromString(keyIdString)
+        } catch (ex: Exception) {
+            return null
+        }
+        val userAccessKey = usersSource.getKey(keyId) ?: return null
+        val user = usersSource.getUser(userAccessKey.userId) ?: return null
+        if (!user.isAuthorized) {
+            return null
+        }
+        if (accessKey != userAccessKey.key) {
+            return null
+        }
+        return UserIdPrincipal(user.email)
+    }
+
     override suspend fun hash(password: String): String {
         val hmac = Mac.getInstance(algorithm)
+        val hmacKey = SecretKeySpec(hashKey, algorithm)
         hmac.init(hmacKey)
         return hex(hmac.doFinal(password.toByteArray()))
     }
