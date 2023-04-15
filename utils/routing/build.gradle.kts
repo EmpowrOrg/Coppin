c933e92fa2d865cfed8687ab2ccd356eac363a12
adevezin <adrianldevezin@mailfence.com>
2023-04-15 14:54:04 -0400

    Remove unecessary dependencies in routing utils

--- utils/routing/build.gradle.kts
+++ utils/routing/build.gradle.kts
@@ -2,8 +2,5 @@ dependencies {
     implementation(Deps.Ktor.auth)
     implementation(Deps.Ktor.core)
     implementation(Deps.Ktor.freeMarker)
-    implementation(Deps.Ktor.json)
-    implementation(Deps.Ktor.contentNegotiation)
-    implementation(project(Deps.Utils.main))
-    implementation(Deps.Kotlin.dateTime)
+    implementation(Deps.Ktor.sessions)
 }
