b925257e4b10ae2b00a5131dea3c2958837e4001
adevezin <adrianldevezin@mailfence.com>
2023-07-02 17:58:32 -0400

    Update Submissions Table to extend BaseTable

--- db/src/main/kotlin/org/empowrco/coppin/db/Submissions.kt
+++ db/src/main/kotlin/org/empowrco/coppin/db/Submissions.kt
@@ -1,13 +1,10 @@
 package org.empowrco.coppin.db
 
-import org.jetbrains.exposed.dao.id.UUIDTable
-
-object Submissions : UUIDTable() {
+object Submissions : BaseTable() {
     val assignment = reference("assignment", Assignments.id)
     val correct = bool("correct")
     val code = text("code")
     val attempt = integer("attempt")
-    val studentEmail = varchar("student_email", ColumnConfig.username)
     val studentId = text("student_id")
 
     init {
