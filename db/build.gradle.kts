6d7e8cb5184b19858206f3a56e25203b6be35835
adevezin <adrianldevezin@mailfence.com>
2023-07-03 16:01:15 -0400

    Add datetime support for db

--- db/build.gradle.kts
+++ db/build.gradle.kts
@@ -1,6 +1,7 @@
 dependencies {
     implementation(Deps.Exposed.core)
     implementation(Deps.Exposed.jdbc)
+    implementation(Deps.Exposed.dateTime)
     implementation(Deps.Hikari.main)
     implementation(Deps.Postgresql.main)
     implementation(project(Deps.Models.main))
