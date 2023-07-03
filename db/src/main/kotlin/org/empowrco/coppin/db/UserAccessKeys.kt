18a383eeb01f346e9ba48e65ef3cd56be3eb2bdb
adevezin <adrianldevezin@mailfence.com>
2023-07-03 16:01:02 -0400

    Add name support for keys

--- db/src/main/kotlin/org/empowrco/coppin/db/UserAccessKeys.kt
+++ db/src/main/kotlin/org/empowrco/coppin/db/UserAccessKeys.kt
@@ -3,8 +3,10 @@ package org.empowrco.coppin.db
 object UserAccessKeys : BaseTable() {
     val user = reference("user", Users.id)
     val key = text("key_hash")
+    val name = varchar("name", ColumnConfig.name)
 
     init {
         uniqueIndex(user, key)
+        uniqueIndex(user, name)
     }
 }
