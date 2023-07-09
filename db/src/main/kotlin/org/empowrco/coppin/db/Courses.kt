e9ee4e1226c87aa1cd7957320e7c0604dc18f9c1
adevezin <adrianldevezin@mailfence.com>
2023-07-09 18:12:23 -0400

    Ensure courses extends BaseTable

--- db/src/main/kotlin/org/empowrco/coppin/db/Courses.kt
+++ db/src/main/kotlin/org/empowrco/coppin/db/Courses.kt
@@ -1,8 +1,6 @@
 package org.empowrco.coppin.db
 
-import org.jetbrains.exposed.dao.id.UUIDTable
-
-object Courses : UUIDTable() {
+object Courses : BaseTable() {
     val edxId = text("edx_id")
     val title = text("title")
     val number = text("number")
