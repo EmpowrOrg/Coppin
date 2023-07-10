0f4640ec3f20d6b1f5f9b692b5acb57d5714da77
adevezin <adrianldevezin@mailfence.com>
2023-07-10 14:10:08 -0400

    Update GetCourseRespones for v1

--- courses/presenters/src/main/kotlin/org/empowrco/coppin/courses/presenters/CoursesPortalResponses.kt
+++ courses/presenters/src/main/kotlin/org/empowrco/coppin/courses/presenters/CoursesPortalResponses.kt
@@ -11,26 +11,17 @@ data class GetCoursesResponse(val courses: List<Course>, val coursesCount: Int) 
 }
 
 data class GetCourseResponse(
+    val id: String,
     val name: String,
     val referenceId: String,
-    val completionRate: String,
-    val students: List<Student>,
-    val gradingSummaries: List<GradingSummary>,
     val assignments: List<Assignment>,
 ) {
-    data class GradingSummary(val type: String, val weight: String, val grade: String, val weightGrade: String)
-    data class Student(
-        val name: String,
-        val lab: String,
-        val homework: String,
-        val finalExam: String,
-        val engagement: String,
-    )
 
     data class Assignment(
         val id: String,
         val title: String,
         val successRate: String,
+        val completionRate: String,
         val lastModified: String,
     )
 }
