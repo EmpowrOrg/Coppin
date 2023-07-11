5469f72cf0bd86aa248d8f23503ce5117cd66a3c
adevezin <adrianldevezin@mailfence.com>
2023-07-11 14:01:37 -0400

    Make edxId on Courses unique

--- db/src/main/kotlin/org/empowrco/coppin/db/Courses.kt
+++ db/src/main/kotlin/org/empowrco/coppin/db/Courses.kt
@@ -1,7 +1,7 @@
 package org.empowrco.coppin.db
 
 object Courses : BaseTable() {
-    val edxId = text("edx_id")
+    val edxId = text("edx_id").uniqueIndex()
     val title = text("title")
     val number = text("number")
     val org = text("org")
