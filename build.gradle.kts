cae86178d2021b8763a78c4f0b72907dbd4614d2
adevezin <adrianldevezin@mailfence.com>
2023-07-27 16:51:41 -0400

    Use ConfigData for debug

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,8 +11,7 @@ group = ConfigData.baseGroup
 version = ConfigData.version
 application {
     mainClass.set("io.ktor.server.netty.EngineMain")
-    val isDevelopment: Boolean = project.ext.has("development")
-    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=${ConfigData.isDebug}")
 }
 
 repositories {
--- buildSrc/src/main/kotlin/ConfigData.kt
+++ buildSrc/src/main/kotlin/ConfigData.kt
@@ -1,4 +1,5 @@
 object ConfigData {
     const val version = "1.0.0rc3"
     const val baseGroup = "org.empowrco.coppin"
+    val isDebug = System.getenv("DEBUG")
 }
