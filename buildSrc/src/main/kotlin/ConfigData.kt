5407dd4715a4eb0e526fd3036d100ba8eac46ad0
adevezin <adrianldevezin@mailfence.com>
2023-07-28 12:33:45 -0400

    Update to 1.0.0rc4

--- buildSrc/src/main/kotlin/ConfigData.kt
+++ buildSrc/src/main/kotlin/ConfigData.kt
@@ -1,5 +1,5 @@
 object ConfigData {
-    const val version = "1.0.0rc3"
+    const val version = "1.0.0rc4"
     const val baseGroup = "org.empowrco.coppin"
     val isDebug = System.getenv("DEBUG")
 }
