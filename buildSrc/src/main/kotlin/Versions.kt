f7acf474e83a1e79ec743b772d0ddc6ceb415da8
adevezin <adrianldevezin@mailfence.com>
2023-08-08 13:47:54 -0400

    Update kotlin and ktor

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,7 +1,7 @@
 object Versions {
     const val jwt = "4.2.2"
-    const val kotlin = "1.7.20"
-    const val ktor = "2.3.2"
+    const val kotlin = "1.9.0"
+    const val ktor = "2.3.3"
     const val lettuce = "6.2.2.RELEASE"
     const val logback = "1.2.10"
     const val koin = "3.2.2"
