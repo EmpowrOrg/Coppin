0015ab611598ebcc4d04d7a04ea1860660912990
adevezin <adrianldevezin@mailfence.com>
2023-10-20 15:05:36 -0400

    Update to rc11

--- buildSrc/src/main/kotlin/ConfigData.kt
+++ buildSrc/src/main/kotlin/ConfigData.kt
@@ -1,5 +1,5 @@
 object ConfigData {
-    const val version = "1.0.0rc10"
+    const val version = "1.0.0rc11"
     const val baseGroup = "org.empowrco.coppin"
     val isDebug = System.getenv("DEBUG")
 }
